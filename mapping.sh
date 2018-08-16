33ffb9333e25baec6885d892eebd2ec3cd3bc624
dmatelska <dmatelska@genesilico.pl>
2018-08-17 00:27:06 +0200

    Bowtie mapping after poly-A trimming

--- mapping.sh
+++ mapping.sh
@@ -9,3 +9,6 @@ bowtie2 -5 1 --fr -k 5 -x hg_mapping/GCA_000001405.15_GRCh38_no_alt_analysis_set
 
 bowtie2 -5 1 -3 35 --fr -k 5 -x hg_mapping/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index -1 barcodes/${C1_R1}_CATGAG_30.fastq.gz -2 barcodes/${C1_R2}_CATGAG_30.fastq.gz --threads 6 -S hg_mapping/${C1_R1}_CATGAG_bowtie.sam 2>> analysis/bowtie.out
 bowtie2 -5 1 -3 35 --fr -k 5 -x hg_mapping/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index -1 barcodes/${NB_R1}_CATGAG_30.fastq.gz -2 barcodes/${NB_R2}_CATGAG_30.fastq.gz --threads 6 -S hg_mapping/${NB_R1}_CATGAG_bowtie.sam 2>> analysis/bowtie.out
+
+
+#fastq_to_fasta -Q33 -i hg_mapping/${NB_R1}_AGACTC_bowtie.sam -o hg_mapping/${NB_R1}_AGACTC_bowtie.fasta
